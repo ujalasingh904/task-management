c1d52a09b80f3c271a29f7f97ebd7d29a324c94c
ujala <singhujala2004@gmail.com>
2026-04-30 13:26:48 +0530

    third commit

--- server/server.js
+++ server/server.js
@@ -22,7 +22,10 @@ app.use(cors({
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
- 
+app.use('/', (req, res) => {
+  res.json({ message: 'Welcome to the Task Management API' });
+});
+
 app.use('/api/auth', authRoutes);
 app.use('/api/projects', projectRoutes);
 app.use('/api/tasks', taskRoutes);
